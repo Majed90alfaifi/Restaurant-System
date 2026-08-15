ef98dbc48cca65cd986ecad3413a66ef33207c51
Majed90alfaifi <99211143+Majed90alfaifi@users.noreply.github.com>
2026-08-15 12:57:52 +0300

    Update DBConnection.java

--- Project252/src/main/java/org/majed/project252/DB/DBConnection.java
+++ Project252/src/main/java/org/majed/project252/DB/DBConnection.java
@@ -8,23 +8,48 @@ public class DBConnection {
 
     private static DBConnection instance;
     private Connection connection;
-    private String url = "jdbc:mysql://localhost:3306/cpit252project_db";
-    private String username = "root";
-    private String password = "root";
+
+    private final String url = getEnv(
+            "DB_URL",
+            "jdbc:mysql://localhost:3306/cpit252project_db"
+    );
+
+    private final String username = getEnv(
+            "DB_USERNAME",
+            "root"
+    );
+
+    private final String password = getEnv(
+            "DB_PASSWORD",
+            ""
+    );
 
     private DBConnection() throws SQLException {
-            this.connection = DriverManager.getConnection(url, username, password);
-
+        connection = DriverManager.getConnection(url, username, password);
     }
+
     public Connection getConnection() {
         return connection;
     }
+
     public static DBConnection getInstance() throws SQLException {
+
         if (instance == null) {
             instance = new DBConnection();
         } else if (instance.getConnection().isClosed()) {
             instance = new DBConnection();
         }
+
         return instance;
+    }
+
+    private static String getEnv(String key, String defaultValue) {
+        String value = System.getenv(key);
+
+        if (value == null || value.isEmpty()) {
+            return defaultValue;
+        }
+
+        return value;
     }
 }
